b08fb5ba682d5568219124d8a50d862bc47baab4
claudiuromcea <romcea_claudiu@yahoo.com>
2019-04-13 13:17:53 +0300

    Update given interfaces

--- Workshop/src/main/java/ro/axonsoft/internship/api/WorkshopFinder.java
+++ Workshop/src/main/java/ro/axonsoft/internship/api/WorkshopFinder.java
@@ -1,4 +1,10 @@
 package ro.axonsoft.internship.api;
 
 public interface WorkshopFinder {
+    /**
+     * Search the maximum set of workshops the student can attend
+     * @param studentDescription the description of the student
+     * @return the result of the search made for the student
+     */
+    SearchResult getWorkshops(StudentDescriptor studentDescriptor);
 }
